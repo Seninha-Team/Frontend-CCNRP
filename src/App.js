5dbe2a58316dbce2a423abaf6f79ae17c33b9e7e
Natan Gbald <natangbaldoino@gmail.com>
2025-10-12 18:29:39 -0300

    CCRNP-33-Implementar-edição-e-exclusão-put-e-delete

--- src/App.js
+++ src/App.js
@@ -30,6 +30,24 @@ function App() {
       .catch(err => console.log(err));
   };
 
+  const atualizarPeca = (peca) => {
+    const novoNome = prompt('Novo nome da peça:', peca.nome);
+    const novaQuantidade = prompt('Nova quantidade:', peca.quantidade);
+    if (!novoNome || !novaQuantidade) return;
+
+    axios.put(`${API_URL}/${peca.id}`, { nome: novoNome, quantidade: Number(novaQuantidade) })
+      .then(res => {
+        setPecas(pecas.map(p => p.id === peca.id ? res.data : p));
+      })
+      .catch(err => console.log(err));
+  };
+
+  const deletarPeca = (id) => {
+    axios.delete(`${API_URL}/${id}`)
+      .then(() => setPecas(pecas.filter(p => p.id !== id)))
+      .catch(err => console.log(err));
+  };
+
 };
 export default App;
   
