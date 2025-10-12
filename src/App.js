d686f2b8a00f006534d7330d40c45ebe92bbde06
Natan Gbald <natangbaldoino@gmail.com>
2025-10-12 17:56:47 -0300

    CCRNP-31-Implementar-Listagem-Get

--- src/App.js
+++ src/App.js
@@ -7,7 +7,18 @@ function App() {
   const [quantidade, setQuantidade] = useState('');
 
   const API_URL = 'http://localhost:3001/pecas';
-};
 
+
+  const listarPecas = () => {
+    axios.get(API_URL)
+      .then(res => setPecas(res.data))
+      .catch(err => console.log(err));
+ };
+
+  useEffect(() => {
+    listarPecas();
+  }, []);
+
+};
 export default App;
   
