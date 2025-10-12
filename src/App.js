7294ab2e06429f85d91cff94917e18a28d6763cb
Natan Gbald <natangbaldoino@gmail.com>
2025-10-12 17:50:39 -0300

    CCRNP-30-Criar-service-.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  
+  const [pecas, setPecas] = useState([]);
+  const [nome, setNome] = useState('');
+  const [quantidade, setQuantidade] = useState('');
 
-}
+  const API_URL = 'http://localhost:3001/pecas';
+};
 
 export default App;
   
